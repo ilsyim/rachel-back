1918a6908fd4572ad4bed51bc3dacaae9ed3c643
ilysim <illymarie1@live.com>
2023-03-29 16:12:16 -0500

    add model

---
+++ models/photo.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const photoSchema = new Schema({
+  photoTitle: {type: String, required: true},
+  photoEvent: {type: String, required: true},
+  photoDate: {type: Date, required: true},
+  owner: {type: mongoose.Schema.Types.ObjectId, ref: "Profile"},
+}, {
+  timestamps: true
+})
+
+const Photo = mongoose.model('Photo', photoSchema)
+
+export {
+  Photo
+}
